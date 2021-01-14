393ec5ee3ce7e92b148a510fb0e379c5808ee728
rihem <rihemebenhassan@insat.u-carthage.tn>
2021-01-14 14:54:37 +0100

    first page UI

--- todo/lib/homepage.dart
+++ todo/lib/homepage.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:todo/Widgets.dart';
 
+// ignore: camel_case_types
 class homepage extends StatefulWidget {
   @override
   _homepageState createState() => _homepageState();
@@ -19,19 +20,37 @@ class _homepageState extends State<homepage> {
             vertical: 32,
           ),
           color: Color(0xFFF6F6F6),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
+          child: Stack(
             children: [
-              Container(
-                margin: EdgeInsets.only(bottom: 32),
-                child: Image(
-                  image: AssetImage('assets/images/logo.png'),
-                ),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Container(
+                    margin: EdgeInsets.only(bottom: 32),
+                    child: Image(
+                      image: AssetImage('assets/images/logo.png'),
+                    ),
+                  ),
+                  TaskCard(title: "First Task", desc: "hello"),
+                  TaskCard(),
+                  TaskCard(),
+                ],
               ),
               //23:43
-              TaskCard(title: "First Task", desc: "hello"),
-              TaskCard(),
-              TaskCard(),
+              Positioned(
+                bottom: 0.0,
+                right: 0.0,
+                child: Container(
+                  width: 60,
+                  height: 60,
+                  decoration: BoxDecoration(
+                      color: Color(0xFF7349FE),
+                      borderRadius: BorderRadius.circular(10)),
+                  child: Image(
+                    image: AssetImage('assets/images/add_icon.png'),
+                  ),
+                ),
+              ),
             ],
           ),
         ),
